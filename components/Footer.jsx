fada28c78c979a2d225777a7754c0f215c5345ee
Mario Fink <mario.fink@goodyear.com>
2021-09-16 22:51:57 +0200

    add icons to footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,70 @@
+const MailIcon = () => (
+  <svg
+    className="w-6 h-6 mr-1"
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+    ></path>
+  </svg>
+);
+
+const HomeIcon = () => (
+  <svg
+    className="w-6 h-6 mr-1"
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+    ></path>
+  </svg>
+);
+
+const PhoneIcon = () => (
+  <svg
+    className="w-6 h-6 mr-1"
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z"
+    ></path>
+  </svg>
+);
+
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-center w-full h-24 border-t">
+    <footer className="flex gap-6 items-center justify-center w-full h-24 border-t">
       <a
-        className="flex items-center justify-center"
+        className="flex text-green-500"
         href="//mariofink.de"
         target="_blank"
         rel="noopener noreferrer"
       >
-        MarioFink.de
+        <HomeIcon /> MarioFink.de
+      </a>
+      <a className="flex text-green-500" href="mailto:info@mariofink.de">
+        <MailIcon /> info@mariofink.de
+      </a>
+      <a className="flex text-green-500" href="tel:1234">
+        <PhoneIcon /> 12345
       </a>
     </footer>
   );
