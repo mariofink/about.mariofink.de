86ba58fd8253dff12ec85b4c50fb3aea667f42f5
Mario Fink <mario.fink@goodyear.com>
2021-09-16 23:34:11 +0200

    add content for tech page

--- pages/tech.js
+++ pages/tech.js
@@ -14,6 +14,26 @@ export default function Tech() {
 
       <main className="py-16 container mx-auto px-6 max-w-prose">
         <h1 className="text-6xl font-bold">Tools & Technologies</h1>
+        <p className="mt-6">
+          The frontend development world is still a fast-changing environment.
+          Today’s hot stuff is tomorrow’s legacy technology.
+        </p>
+        <h2 className="mt-6 mb-3 text-3xl font-bold">Working with right now</h2>
+        <ul className="list-disc list-inside">
+          <li>React</li>
+          <li>Redux & Redux Toolkit</li>
+          <li>Webpack</li>
+          <li>Adobe Experience Manager</li>
+        </ul>
+        <h2 className="mt-6 mb-3 text-3xl font-bold">
+          Worked with in the past
+        </h2>
+        <ul className="list-disc list-inside">
+          <li>VueJS</li>
+          <li>Ionic</li>
+          <li>jQuery</li>
+          <li>Grunt</li>
+        </ul>
       </main>
 
       <Footer />
