f845f087951cb0999925590f7c79b4c5054e7dee
Mario Fink <mario.fink@goodyear.com>
2021-09-16 23:38:12 +0200

    change style of active nav item

--- components/NavItem.jsx
+++ components/NavItem.jsx
@@ -4,12 +4,10 @@ import { useRouter } from "next/router";
 export default function NavItem({ caption, href }) {
   const router = useRouter();
   const activeClasses =
-    router.asPath === href ? "border-b-4 border-green-500 " : "";
+    router.asPath === href ? "border-b-4 border-green-500 " : "text-green-500";
   return (
     <Link href={href}>
-      <a className={`py-4 px-2 text-green-500 font-semibold ${activeClasses}`}>
-        {caption}
-      </a>
+      <a className={`py-4 px-2 font-semibold ${activeClasses}`}>{caption}</a>
     </Link>
   );
 }
