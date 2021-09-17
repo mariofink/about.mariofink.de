20db7654213b98bd76f93696ecf252be051e821f
Mario Fink <mario.fink@goodyear.com>
2021-09-17 11:53:54 +0200

    remove phone

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -34,23 +34,6 @@ const HomeIcon = () => (
   </svg>
 );
 
-const PhoneIcon = () => (
-  <svg
-    className={iconBaseStyle}
-    fill="none"
-    stroke="currentColor"
-    viewBox="0 0 24 24"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z"
-    ></path>
-  </svg>
-);
-
 const GithubIcon = () => (
   <svg
     className={`${iconBaseStyle} fill-current text-green-500`}
@@ -103,9 +86,6 @@ const Footer = () => (
     </a>
     <a className="flex text-green-500 py-1" href="mailto:info@mariofink.de">
       <MailIcon /> info@mariofink.de
-    </a>
-    <a className="flex text-green-500 py-1" href="tel:1234">
-      <PhoneIcon /> 12345
     </a>
   </footer>
 );
