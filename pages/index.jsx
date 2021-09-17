cc9384271e3bc12393f9aaa73220c7c63e5b2b1a
Mario Fink <mario.fink@goodyear.com>
2021-09-17 21:41:12 +0200

    use relaxed leading

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,10 @@ import Footer from "../components/Footer";
 import Heading from "../components/Heading";
 import SubHeading from "../components/Subheading";
 
+const Paragraph = ({ children }) => (
+  <p className="mt-3 text-xl leading-relaxed">{children}</p>
+);
+
 const Home = () => (
   <div className="container mx-auto">
     <DocumentHead title="Mario Fink • About Me" favicon="👋🏻" />
@@ -12,30 +16,30 @@ const Home = () => (
 
     <main className="py-8 container mx-auto px-6 max-w-prose">
       <Heading>About Me</Heading>
-      <p className="mt-3 text-xl">
+      <Paragraph>
         Hi, my name is Mario Fink 👋🏻
         <br /> I live in the beautiful Rhön (a rural area in the midst of
         Germany) together with my wife and my two sons. Currently I work as a
         lead frontend developer at Goodyear Dunlop Tires in Fulda.
-      </p>
+      </Paragraph>
       <SubHeading>Education</SubHeading>
-      <p className="mt-3 text-xl">
+      <Paragraph>
         I finished my technical diploma in Economics & Management in 1999. After
         spending a year in community service (aka. Zivildienst) I started an
         apprenticeship as IT specialist at Goodyear Dunlop Tires that I finished
-        successfully in 2003. I then accepted a job offering at the same company
-        to work on their B2B shop as a web developer.
-      </p>
+        successfully in 2003. Thereafter I accepted a job offering at the same
+        company to work on its B2B shop as a web developer.
+      </Paragraph>
 
       <SubHeading>Work Experience</SubHeading>
-      <p className="mt-3 text-xl">
+      <Paragraph>
         Since 2003 I have been working in a 10-person team as lead frontend
         developer and UX designer – creating a B2B shop that is used throughout
         Europe, Middle East and South Africa (EMEA). My main responsibilities
         are the design and the implementation of the user interface. While doing
         that I am communicating with our European business stakeholders as well
         as our near and off-shore development partners.
-      </p>
+      </Paragraph>
     </main>
 
     <Footer />
