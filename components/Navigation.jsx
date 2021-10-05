b95ccb36b1e00e57d8b3d5e472e33fcc1a9210ba
Mario Fink <mario.fink@goodyear.com>
2021-10-05 22:22:49 +0200

    fix 404s when deploying to static hosting

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -3,8 +3,8 @@ import NavItem from "./NavItem";
 const Navigation = () => (
   <nav className="flex justify-center sm:gap-6 sm:px-6 mt-3">
     <NavItem caption="About Me" href="/" />
-    <NavItem caption="CV/Resume" href="/cv" />
-    <NavItem caption="Tools & Tech" href="/tech" />
+    <NavItem caption="CV/Resume" href="/cv/" />
+    <NavItem caption="Tools & Tech" href="/tech/" />
   </nav>
 );
 
