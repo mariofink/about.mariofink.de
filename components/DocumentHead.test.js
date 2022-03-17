29d7ef62ea562738e619b9888d3fd5f2906590c9
Mario Fink <mario.fink@goodyear.com>
2022-03-17 23:43:12 +0100

    fix tests

--- components/DocumentHead.test.js
+++ components/DocumentHead.test.js
@@ -3,7 +3,7 @@ import DocumentHead from "./DocumentHead";
 describe("DocumentHead", () => {
   it("gets passed title and favicon", () => {
     const underTest = DocumentHead({ title: "mytitle", favicon: "icon" });
-    const [title, link] = underTest.props.children;
+    const [html, title, meta, link] = underTest.props.children;
     expect(title).toBeDefined();
     expect(title.props.children).toEqual("mytitle");
     expect(link).toBeDefined();
