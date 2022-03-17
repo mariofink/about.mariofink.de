f36564104ab36de8dc1a1480e7d4c01a27b6f8d7
Mario Fink <mario.fink@goodyear.com>
2022-03-17 23:33:36 +0100

    add meta description

--- components/DocumentHead.tsx
+++ components/DocumentHead.tsx
@@ -12,6 +12,7 @@ const DocumentHead = ({ title, favicon }: props) => (
   <Head>
     <html lang="en" />
     <title>{title}</title>
+    <meta name="description" content="Mario Fink • Web developer from Fulda" />
     <link
       rel="icon"
       href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${favicon}</text></svg>`}
