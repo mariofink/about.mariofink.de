f123e5ea3e45f61bd378b5b7cd9c78073338ae5c
Mario Fink <mario.fink@goodyear.com>
2022-03-17 23:26:05 +0100

    remove tailwind warnings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
@@ -9,4 +11,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
